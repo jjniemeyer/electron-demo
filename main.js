96617130487be247c2e68d7edcab67e98a993e32
Jasper Niemeyer <jani5714@colorado.edu>
2023-12-13 13:40:07 -0700

    open a window if none are open (macOS)

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
 app.on('window-all-closed', () => {
