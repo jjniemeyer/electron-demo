1dd0b3d3faa9e31c4d8f74c4e41ba9a24e945811
Jasper Niemeyer <jani5714@colorado.edu>
2023-12-13 13:41:27 -0700

    prettier

--- main.js
+++ main.js
@@ -13,10 +13,14 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+      }
   })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
