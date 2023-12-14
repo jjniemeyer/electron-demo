0cd0ab4b5cd1ff47167b6940e7306a080bf027c3
Jasper Niemeyer <jani5714@colorado.edu>
2023-12-14 10:47:40 -0700

    set up preload and renderer

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
@@ -13,14 +17,14 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) {
-          createWindow()
-      }
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
   })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
