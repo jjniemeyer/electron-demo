630e1a88793a777c7af20eb1391c42d4567af761
Jasper Niemeyer <jani5714@colorado.edu>
2023-12-14 15:40:21 -0700

    ipc demo

--- renderer.js
+++ renderer.js
@@ -1,2 +1,10 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const pingElement = document.getElementById('pingPong')
+const pingPong = async () => {
+    const response = await window.versions.ping()
+    pingElement.innerText = `response from ipc ping: ${response}`
+}
+
+pingPong()
